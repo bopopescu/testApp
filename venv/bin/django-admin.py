e16ca2865beeadba028ad2f1add065400d5e61b3
subhrangshu <lalshu.rnj@gmail.com>
2020-05-18 23:45:16 +0530

    First Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/subhrangshu/IdeaProjects/Web-App/GS_Enterprise_Backend_Server/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
